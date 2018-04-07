d90ba8b2955222fd6d0f3f32fc7ba9bfba1ae1ea
Louis Faul <louis.faul@imt-atlantique.net>
2018-04-07 13:26:10 +0200

    2 test reussis

--- c2w/protocol/udp_chat_server.py
+++ c2w/protocol/udp_chat_server.py
@@ -2,6 +2,8 @@
 from twisted.internet.protocol import DatagramProtocol
 from c2w.main.lossy_transport import LossyTransport
 import logging
+import struct
+import random
 
 logging.basicConfig()
 moduleLogger = logging.getLogger('c2w.protocol.udp_chat_server_protocol')
@@ -65,61 +67,84 @@ class c2wUdpChatServerProtocol(DatagramProtocol):
         Twisted calls this method when the server has received a UDP
         packet.  You cannot change the signature of this method.
         """
-        Packet=struct.unpack('>BBHHH'+str(len(datagram-8))+'s',datagram) 
-        Version=Packet[0]//2**4
-        Type=Packet[0]-Packet[0]//2**4
+        Packet=struct.unpack('>BBHHH'+str(len(datagram)-8)+'s',datagram) 
+        Version=Packet[0]//(2**4)
+        Type=Packet[0]-(2**4)*(Packet[0]//(2**4))
         SessionToken=Packet[1]*(2**16)+Packet[2]
         SequenceNumber=Packet[3]
-        len(Payload)=Packet[4]
+        PayloadSize=Packet[4]
         Payload=Packet[5]
 
-       if Type=0:
-          self.tokenSequenceNumber.remove(SessionToken,SequenceNumber)
-       if SequenceNumber and SessionToken not in self.SequenceTokenList: 
+        #if Type==0:
+          #self.tokenSequenceNumber.remove(SessionToken,SequenceNumber)
+        #if SequenceNumber and SessionToken not in self.SequenceTokenList: 
 
-           if Type!=0:## if the datagram isn't an ACK we have to send one to the client
+        if Type!=0:## if the datagram isn't an ACK we have to send one to the client
             
-               Ack=struct.pack('>BBBHHH',1,0,SessionToken//(2**16),SessionToken-(2**16)*(SessionToken//(2**16)),SequenceNumber,0)
-               self.transport.write(Ack,host_port)
+            Ack=struct.pack('>BBHHH',16,SessionToken//(2**16),SessionToken-(2**16)*(SessionToken//(2**16)),SequenceNumber,0)
+            self.transport.write(Ack,host_port)
 
-           if Type==1 :
+        if Type==1 :
 
-               uData=struct.unpack('>H'+str(len(Payload))+'s',Payload)
-               uName=struct.unpack(str(len(uData[1]))+'s',uData[1])
-               userName=uName[1].decode('utf-8') 
-          
-               if len(userName)>100:
-                   ResponseCode=2
-                   uData=struct.pack('>H'+str(len(uData[1]))+'s',0,uData[1])
+            uData=struct.unpack('>H'+str(len(Payload)-2)+'s',Payload)
+            uName=struct.unpack('H'+str(len(uData[1])-2)+'s',uData[1])
+            userName=uName[1].decode('utf-8') 
+         
+            if len(userName)>100:
+                ResponseCode=2
+                uData=struct.pack('>H'+str(len(uData[1]))+'s',0,uData[1])
 
-               elif self.serverProxy.userExists(userName)==True:
-                   ResponseCode=3
-                   uData=struct.pack('>H'+str(len(uData[1]))+'s',0,uData[1])
+            elif self.serverProxy.userExists(userName)==True:
+                ResponseCode=3
+                uData=struct.pack('>H'+str(len(uData[1]))+'s',0,uData[1])
             
            
-               elif len(serverProxy.getUserList())>65535:
-                   ResponseCode=4
-                   uData=struct.pack('H'+str(len(uData[1]))+'s',0,uData[1])
+            elif len(self.serverProxy.getUserList())>65535:
+                ResponseCode=4
+                uData=struct.pack('H'+str(len(uData[1]))+'s',0,uData[1])
 
-               else:
-                   ResponseCode=0
-                   userID=1
-                   while not(self.ServerProxy.getUserByID(userID)):
-                        userID+=1
+            else:
+                ResponseCode=0
+                userID=1
+                #while not(self.serverProxy.getUserById(userID)):
+                #userID+=1
 
-                   uData=struct.pack('>H'+str(len(uData[1]))+'s',userID,uData[1])
-                   SessionToken=random.getrandbits(24)
-               Version=1
-               Type=2
-               Payload=struct.pack('>B'+str(len(uData))+'s',ResponseCode,uData)
-               LoginResponse=struct.pack('>BBHHH'+str(len(Payload)+'s',Version*2**4+Type,SessionToken//(2**16),SessionToken-(2**16)*(SessionToken//(2**16)),SequenceNumber,len(Payload),Payload)
-               self.transport.write(LoginResponse,host_port)
-               self.tokenSequenceList.append(SessionToken)
-               self.tokenSequenceList.append(SequenceNumber)
-               
+                uData=struct.pack('>H'+str(len(uData[1]))+'s',userID,uData[1])
+                SessionToken=random.getrandbits(24)
+            Version=1
+            Type=2
+            Payload=struct.pack('>B'+str(len(uData))+'s',ResponseCode,uData)
+            LoginResponse=struct.pack('>BBHHH'+str(len(Payload))+'s',Version*2**4+Type,SessionToken//(2**16),SessionToken-(2**16)*(SessionToken//(2**16)),SequenceNumber,len(Payload),Payload)
+            self.transport.write(LoginResponse,host_port)
+            #self.tokenSequenceList.append(SessionToken)
+            #self.tokenSequenceList.append(SequenceNumber)
+            #Packet=struct.unpack('>BBHHH'+str(len(datagram)-8)+'s',datagram)
+            #Version=Packet[0]//2**4
+            #Type=Packet[0]-Packet[0]//2**4
+            #SessionToken=Packet[1]*(2**16)+Packet[2]
+            #SequenceNumber=Packet[3]
+            #PayloadSize=Packet[4]
+            #Payload=Packet[5]
+            def RST(
+            #self.serverProxy.addUser(userName,'Main Room',None,host_port)
+            #SequenceNumber+=1
+            #RoomIdentifier=1 
+            #RoomName=struct.pack('>H9s',9,'Main Room'.encode('utf-8'))
+            #MovieIP=0
+            #MoviePort=0
+            #UsersMainRoom=[]
+            #for user in self.serverProxy.getUserList():
+                #if user.userChatRoom=='Main Room':
+                        #UsersMainRoom.append(struct.pack('>HH'+str(len(user.userName))+'s',user.userId,len(user.userName),userName.encode('utf-8')))
+          #NumberOfFilms=len(self.serverProxy.getUserList())
+          #for Movie in self.serverProxy.getUserList():
+              #RoomId=self.serverProxy.getMovieAddrPort(Movie)
+              #RoomName=self.serverProxy.
+         #length UsersMainRoom
+#Room List : number of films available , pour chaque film Room Identifier , Room Name , MovieIP , MoviePort
 
-          
 
+                 
             
             
         pass
